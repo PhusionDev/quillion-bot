52b8779e5e94a3387b979a5dbadef8eb3cce8637
Jared Hodgkins <phusiondev@gmail.com>
2022-04-03 01:24:27 -0400

    Added rolecheck admin command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 from google.oauth2.service_account import Credentials
 from discord.ext import commands
@@ -6,7 +7,10 @@ import discord
 import gspread
 import datetime
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
@@ -22,10 +26,33 @@ sh = sa.open("Quillion CRO Whitelist")
 
 wks_db = sh.worksheet("UUID Table")
 wks_db_records = {}
+
+admins = {'150380581723701250': True}
 names = {}
 uuids = {}
+rolemembers = []
+
 last_db_datetime = datetime.datetime.now() - datetime.timedelta(hours=1)
 fetching = False
+
+# Internal Functions
+
+# Utility / Helper
+def is_admin(user_id):
+  if str(user_id) in admins:
+    return admins[str(user_id)]
+
+def get_value(str):
+  print(f'Getting value for: {str}')
+  str = strip_at_bang(str)
+  print(f'Getting value for: {str}')
+  try:
+    return int(str)
+  except ValueError:
+    return None
+
+def strip_at_bang(ab_str):
+  return re.sub('[<>@!]*','',ab_str)
 
 def update_db():
   global last_db_datetime
@@ -67,6 +94,9 @@ def get_name(user_id):
     print(f'{user_id}\'s name: {name}')
   return name
 
+# BOT COMMANDS
+
+# Whitelist Command
 @bot.command()
 async def WL(ctx):
   message = None
@@ -81,5 +111,27 @@ async def WL(ctx):
       await ctx.author.send(message)
     except discord.Forbidden:
       pass
+
+# Role Check Command
+@bot.command()
+async def rolecheck(ctx, *, role="Hedgies WL (CRO)"):
+  global rolemembers
+  if is_admin(ctx.author.id):
+    update_db()
+    role = strip_at_bang(role)
+    rolemembers = []
+    print(f'searching for role: {role}')
+    for member in ctx.guild.members:
+      # print(f'Checking if {member.name} belongs to {role} role')
+      for r in member.roles:
+          if r.name == role:
+            names[str(member.id)] = member.name
+            rolemembers.append(str(member.id))
+    # print(f'Members in {role} role:\n{rolemembers}')
+    count_roles = len(rolemembers)
+    count_names = len(names)
+    ctx.message.channel.send(f'Members in WL DB: {count_names} | Members with {role} role: {count_roles}')
+  else:
+    print(f'User: {ctx.author.id} is not authorized')
 
 bot.run(TOKEN)
