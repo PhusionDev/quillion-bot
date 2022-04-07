f9db046ef9ba629355b53e2faf30d74f3d20d89f
Jared Hodgkins <phusiondev@gmail.com>
2022-04-07 14:08:52 -0400

    Added ability for bot to tell user their # of valid entries and information

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import re
 import json
 import datetime
-# from google.oauth2.service_account import Credentials
 import gspread
 import nextcord
 from nextcord import Interaction
@@ -31,13 +30,18 @@ wks_db_records = {}
 wks_config = sh.worksheet("Config")
 wks_config_records = {}
 
+wks_valid_entries = sh.worksheet("Parsed Responses")
+wks_valid_entries_records = {}
+
 guilds = [937228523964346440]
 admins = {150380581723701250: True}
+valid_entries = {}
 names = {}
 uuids = {}
 rolemembers = []
 newmembers = {}
 retiredmembers = {}
+num_entries = {}
 
 last_db_datetime = datetime.datetime.now() - datetime.timedelta(hours=1)
 fetching = False
@@ -68,14 +72,62 @@ def generate_csv(dict, title="Dict to CSV"):
   csv_str += f'```'
   return csv_str
 
+def update_uuid_records(records_uuids):
+  global uuids
+  global names
+  global wks_db_records
+  if wks_db_records != records_uuids:
+    wks_db_records = records_uuids
+    uuids = {}
+    names = {}
+    print("Debug: UUID Records list has changed")
+    for d in wks_db_records:
+      if not d['ID'] == "":
+        uuids[d['ID']] = d['UUID']
+        names[d['ID']] = d['Name']
+    print(f'uuids: {len(uuids.keys())} | names: {len(names.keys())}')
+
+def update_config_records(records_config):
+  global admins
+  global wks_config_records
+  if wks_config_records != records_config:
+    wks_config_records = records_config
+    new_admins = {}
+    print("Configuration settings have changed")
+    for d in wks_config_records:
+      if not d['admin_id'] == "":
+        new_admins[d['admin_id']] = True
+    admins = new_admins
+    if not 150380581723701250 in admins:
+      admins[150380581723701250] = True
+    print(f'# of admins: {len(admins)} | Admins List:\n{admins}')
+
+def update_valid_entries_records(records_valid_entries):
+  global wks_valid_entries_records
+  global valid_entries
+  global num_entries
+  if wks_valid_entries_records != records_valid_entries:
+    wks_valid_entries_records = records_valid_entries
+    valid_entries = {}
+    num_entries = {}
+    print(f'Valid entries have changed')
+    for d in wks_valid_entries_records:
+      if not d['user id'] == "":
+        id = d['user id']
+        if not id in num_entries:
+          num_entries[id] = 1
+        else:
+          num_entries[id] += 1
+        info_dict = {}
+        info_dict["name"] = d["Discord User"]
+        info_dict["wallet"] = d["Address"]
+        info_dict["qty"] = d["Qty"]
+        valid_entries[id] = info_dict
+    print(f'Valid Entries: {len(valid_entries)}')
+
 def update_db():
   global last_db_datetime
   global fetching
-  global wks_db_records
-  global wks_config_records
-  global names
-  global uuids
-  global admins
   dt = datetime.datetime.now()
   dd = dt - last_db_datetime
   if (dd.seconds > 60):
@@ -84,31 +136,13 @@ def update_db():
       print("Debug: last db update over 60s ago, fetching data")
       records_db = wks_db.get_all_records()
       records_config = wks_config.get_all_records()
+      records_valid_entries = wks_valid_entries.get_all_records()
       last_db_datetime = dt
       fetching = False
-      if wks_db_records != records_db:
-        wks_db_records = records_db
-        uuids = {}
-        names = {}
-        print("Debug: UUID Records list has changed")
-        for d in wks_db_records:
-          if not d['ID'] == "":
-            uuids[d['ID']] = d['UUID']
-            names[d['ID']] = d['Name']
-        print(f'uuids: {len(uuids.keys())} | names: {len(names.keys())}')
-      # check configuration updates
-      if wks_config_records != records_config:
-        wks_config_records = records_config
-        new_admins = {}
-        print("Configuration settings have changed")
-        for d in wks_config_records:
-          if not d['admin_id'] == "":
-            new_admins[d['admin_id']] = True
-        admins = new_admins
-        if not 150380581723701250 in admins:
-          admins[150380581723701250] = True
-        print(f'# of admins: {len(admins)} | Admins List:\n{admins}')
-
+      update_uuid_records(records_db)
+      update_config_records(records_config)
+      update_valid_entries_records(records_valid_entries)
+      
 def get_uuid(user_id):
   uuid = None
   update_db()
@@ -131,34 +165,69 @@ def has_role(member):
       return True
   return False
 
+def wl_sorry(name):
+  message = f'Sorry, {name}, but you are not on the whitelist! :x:\nPlease stay tuned for opportunities to join the whitelist!'
+  return message
+
+def wl_info(uuid):
+  message = f':page_facing_up: Whitelist Form :page_facing_up:\nhttps://forms.gle/VbEbptp6zq1RPns59\n\n:closed_lock_with_key: Your Verify Code :closed_lock_with_key:\n```{uuid}```\n:exclamation: **Please do not share this code or your entry may be invalidated!** :exclamation:'
+  return message
+
+def wl_waiting_db(name):
+  message = f'Hello {name},\nYou have the WL role :white_check_mark:\nYou are on the whitelist :white_check_mark:\nBut your information has not been entered into the database yet :x:\nWe are updating the DB regularly as users earn the role\nPlease try again in a little while or message an admin'
+  return message
+
+def wl_greeting_new(name):
+  message = f'`Hello {name}! Here is your information for Quillion\'s Cronos Whitelist:`'
+  return message
+
+def wl_greeting_existing(name, id):
+  num = num_entries[id]
+  entry_str = "entry" if num == 1 else "entries"
+  message = f'Hello {name} you have already submitted **{num_entries[id]}** valid {entry_str}.\nYour most recent submission will be used:'
+  return message
+
 # BOT COMMANDS
 
 @bot.slash_command(name="whitelist", description="get whitelist information", guild_ids=guilds)
 async def wl(interaction: Interaction):
-  message='Sorry, but you are not on the whitelist! :x:\nPlease stay tuned for opportunities to join the whitelist!'
+  message=wl_sorry(interaction.user.name)
   if has_role(interaction.user):
     uuid = get_uuid(interaction.user.id)
-    if (uuid):
-      message = f'`Hello {interaction.user.name}! Here is your information for Quillion\'s Cronos Whitelist:`\n\n:page_facing_up: Whitelist Form :page_facing_up:\nhttps://forms.gle/VbEbptp6zq1RPns59\n\n:closed_lock_with_key: Your Verify Code :closed_lock_with_key:\n```{uuid}```\n:exclamation: **Please do not share this code or your entry may be invalidated!** :exclamation:'
+    if interaction.user.id in valid_entries:
+      id = interaction.user.id
+      name = valid_entries[id]["name"]
+      wallet = valid_entries[id]["wallet"]
+      qty = valid_entries[id]["qty"]
+      message = f'{wl_greeting_existing(interaction.user.name, id)}\n```Name: {name}\nWallet Address: {wallet}\nQty: {qty}```\n\n`If you need to make changes, please submit another entry.`\n\n{wl_info(uuid)}'
     else:
-      message = f'You have the WL role :white_check_mark:\nYou are on the whitelist :white_check_mark:\nBut your information has not been entered into the database yet :x:\nWe are updating the DB regularly as users earn the role\nPlease try again in a little while or message an admin'
+      if (uuid):
+        message = f'{wl_greeting_new(interaction.user.name)}\n\n{wl_info(uuid)}'
+      else:
+        message = wl_waiting_db(interaction.user.name)
   await interaction.response.send_message(message, ephemeral=True)
 
 # Whitelist Command
 @bot.command()
 async def WL(ctx):
-  message = None
-  if (ctx.message.channel.name == '🦔︱wl-bot'):
-    print(f'user id: {ctx.author.id}')
+  message=wl_sorry(ctx.author.name)
+  if has_role(ctx.author):
     uuid = get_uuid(ctx.author.id)
-    if (uuid):
-      message = f'`Hello {ctx.author.name}! Here is your information for Quillion\'s Cronos Whitelist:`\n\n:page_facing_up: Whitelist Form :page_facing_up:\nhttps://forms.gle/VbEbptp6zq1RPns59\n\n:closed_lock_with_key: Your Verify Code :closed_lock_with_key:\n```{uuid}```\n:exclamation: **Please do not share this code or your entry may be invalidated!** :exclamation:'
+    if ctx.author.id in valid_entries:
+      id = ctx.author.id
+      name = valid_entries[id]["name"]
+      wallet = valid_entries[id]["wallet"]
+      qty = valid_entries[id]["qty"]
+      message = f'{wl_greeting_existing(ctx.author.name, id)}\n```Name: {name}\nWallet Address: {wallet}\nQty: {qty}```\n\n`If you need to make changes, please submit another entry.`\n\n{wl_info(uuid)}'
     else:
-      message = f'Sorry you are not on the whitelist!'
-    try:
-      await ctx.author.send(message)
-    except nextcord.Forbidden:
-      pass
+      if (uuid):
+        message = f'{wl_greeting_new(ctx.author.name)}\n\n{wl_info(uuid)}'
+      else:
+        message = wl_waiting_db(ctx.author.name)
+  try:
+    await ctx.author.send(message)
+  except nextcord.Forbidden:
+    pass
 
 # Role Check Command
 @bot.command()
